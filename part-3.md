2f58f7212d67d9b3f007dc14c8fd01bc9a1bbcca
bernard chisumo <bchisumo74@gmail.com>
2023-02-14 21:38:30 +0000

    Implementted the Vue Router for navigation

---
+++ part-3.md
@@ -0,0 +1,18 @@
+## Vue Router for Navigation
+    - To navigate btn one page to another
+    - When User click  links will navigate to another page
+    - Open the app.blade.php
+        Add the following links 
+                    Dashboard
+                    Appointments
+                    Users
+                    Settings
+                    Profile
+                    Logout
+    - Add the routes in routes.js
+    - Create a folder called pages inside js
+        copy from the dashboard.vue
+    - To prevent full page reload , vue link
+         <router-link to=""></router-lnk>
+    - Add the active class dynamically  
+            active-class="active"
--- resources/js/pages/appointments/ListAppointments.vue
+++ resources/js/pages/appointments/ListAppointments.vue
@@ -3,21 +3,20 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard</h1>
+                    <h1 class="m-0">Appointments</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item active">Dashboard</li>
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Appointments</li>
                     </ol>
                 </div>
             </div>
         </div>
     </div>
 
-
     <div class="content">
         <div class="container-fluid">
-            
 
         </div>
     </div>
@@ -25,7 +24,7 @@
 
 <script>
 export default {
-    name: "Dashboard"
+    name: "ListAppointments"
 }
 </script>
 
